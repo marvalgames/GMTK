e49899121b8d850580572793bd2d83cc21199fd4
Francisco Ramos <blbramos@gmail.com>
2024-03-28 01:57:11 -0400

    splatmap terrain to mesh texture

---
+++ Assets/Amazing Assets/All Terrain Textures/Example Scenes/Files/Scripts/ExportBasemap.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using AmazingAssets.AllTerrainTextures;
+
+namespace AmazingAssets.AllTerrainTextures.Examples
+{
+    public class ExportBasemap : MonoBehaviour
+    {
+        public TerrainData terrainData;
+
+        [Space]
+        public bool enableHeightBasedBlend = false;
+        [Range(0f, 1f)]
+        public float heightTransition = 0;
+
+
+        Texture2D basemapDiffuse;
+        Texture2D basemapNormal;
+        
+
+        void OnDisable()
+        {
+            if (basemapDiffuse != null)
+            {
+                if (Application.isEditor)
+                    GameObject.DestroyImmediate(basemapDiffuse);
+                else
+                    GameObject.Destroy(basemapDiffuse);
+            }
+
+            if (basemapNormal != null)
+            {
+                if (Application.isEditor)
+                    GameObject.DestroyImmediate(basemapNormal);
+                else
+                    GameObject.Destroy(basemapNormal);
+            }
+        }
+
+        void Start()
+        {
+            basemapDiffuse = terrainData.AllTerrainTextures(true, false).Basemap.Diffuse(0, enableHeightBasedBlend, heightTransition);
+            basemapNormal = terrainData.AllTerrainTextures(true, false).Basemap.Normal(0, enableHeightBasedBlend, heightTransition);
+
+
+            Material material = GetComponent<MeshRenderer>().material;
+
+            material.SetTexture("_MainTex", basemapDiffuse);
+            material.SetTexture("_BumpMap", basemapNormal);
+        }
+    }
+}
---
+++ Assets/Amazing Assets/All Terrain Textures/Example Scenes/Files/Scripts/ExportBasemapAndSplit.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using AmazingAssets.AllTerrainTextures;
+
+namespace AmazingAssets.AllTerrainTextures.Examples
+{
+    public class ExportBasemapAndSplit : MonoBehaviour
+    {
+        public TerrainData terrainData;
+
+        [Space(5)]
+        public int splitCountX = 3;
+        public int splitCountY = 3;
+
+        [Space(5)]
+        public int positionX = 0;
+        public int positionY = 0;
+
+        [Space]
+        public bool enableHeightBasedBlend = false;
+        [Range(0f, 1f)]
+        public float heightTransition = 0;
+
+
+        Texture2D basemapDiffuse;
+        Texture2D basemapNormal;
+                
+
+        void OnDisable()
+        {
+            if (basemapDiffuse != null)
+            {
+                if (Application.isEditor)
+                    GameObject.DestroyImmediate(basemapDiffuse);
+                else
+                    GameObject.Destroy(basemapDiffuse);
+            }
+
+            if (basemapNormal != null)
+            {
+                if (Application.isEditor)
+                    GameObject.DestroyImmediate(basemapNormal);
+                else
+                    GameObject.Destroy(basemapNormal);
+            }
+        }
+
+        void Start()
+        {
+            basemapDiffuse = terrainData.AllTerrainTextures(true, false).Basemap.Diffuse(0, splitCountX, splitCountY, positionX, positionY, enableHeightBasedBlend, heightTransition);
+            basemapNormal = terrainData.AllTerrainTextures(true, false).Basemap.Normal(0, splitCountX, splitCountY, positionX, positionY, enableHeightBasedBlend, heightTransition);
+
+
+            Material material = GetComponent<MeshRenderer>().material;
+
+            material.SetTexture("_MainTex", basemapDiffuse);
+            material.SetTexture("_BumpMap", basemapNormal);
+        }
+    }
+}
