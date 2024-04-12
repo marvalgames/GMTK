3078fa006390c31b2d3469ab26d973ae0e3d794c
Francisco Ramos <blbramos@gmail.com>
2024-04-12 10:40:09 -0400

    .

--- Assets/Scripts/Player/PlayerMove.cs
+++ Assets/Scripts/Player/PlayerMove.cs
@@ -7,38 +7,11 @@ namespace Sandbox.Player
     
     public class PlayerMove : MonoBehaviour
     {
-        public Entity linkedEntity;
-        public EntityManager entityManager;
-        [HideInInspector]
-        public int targetFrameRate = -1;
 
-
-        // void Start()
-        // {
-        //     if (linkedEntity == Entity.Null)
-        //     {
-        //         linkedEntity = GetComponent<CharacterEntityTracker>().linkedEntity;
-        //         if (entityManager == default)
-        //         {
-        //             entityManager = GetComponent<CharacterEntityTracker>().entityManager;
-        //         }
-        //     }
-        //     
-        //     
-        //
-        //     if (targetFrameRate >= 10)
-        //     {
-        //         Application.targetFrameRate = targetFrameRate;
-        //     }
-        //
-        //     
-        //  
-        //     if (linkedEntity != Entity.Null)
-        //     {
-        //
-        //     }
-        // }
-
+        public void OnFootstep()
+        {
+            
+        }
       
     }
 }
