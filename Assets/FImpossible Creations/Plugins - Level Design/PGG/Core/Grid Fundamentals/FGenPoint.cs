762fb39330e99bcc95efabd36d2c367a540581f0
Francisco Ramos <blbramos@gmail.com>
2024-04-28 20:43:56 -0400

    cleanup

--- Assets/FImpossible Creations/Plugins - Level Design/PGG/Core/Grid Fundamentals/FGenPoint.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-
-namespace FIMSpace.Generating
-{
-    public class FGenPoint
-    {
-        public Vector3Int Pos;
-    }
-}
--- Assets/FImpossible Creations/Plugins - Level Design/PGG/Core/Helper Components/InteriorIDTag.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class InteriorIDTag : MonoBehaviour
-{
-    public int ID = -1;
-}
