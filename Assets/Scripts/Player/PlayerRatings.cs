a6ad14443b96dd8f3859bef057443b232ab17a21
Francisco Ramos <blbramos@gmail.com>
2024-06-27 08:58:53 -0400

    .

--- Assets/Scripts/Player/PlayerRatings.cs
+++ Assets/Scripts/Player/PlayerRatings.cs
@@ -9,10 +9,10 @@ namespace Sandbox.Player
         public PlayerRatingsScriptableObject Ratings;
         public float meleeWeaponPower = 1;
         public float hitPower = 10;//punch kick
+        public float speed = 12;
 
 
 
-       
 
 
     }
@@ -22,13 +22,17 @@ namespace Sandbox.Player
         public override void Bake(PlayerRatings authoring)
         {
             var e = GetEntity(authoring.gameObject, TransformUsageFlags.Dynamic);
+            
+            Debug.Log("Player Ratings " + authoring.speed);
 
             AddComponent( e,
                     new RatingsComponent
                     {
                         tag = 1, maxHealth = authoring.Ratings.maxHealth, 
-                        speed = authoring.Ratings.speed,
-                        gameSpeed =  authoring.Ratings.speed,
+                        //speed = authoring.Ratings.speed,
+                        //gameSpeed =  authoring.Ratings.speed,
+                        speed = authoring.speed,
+                        gameSpeed =  authoring.speed,
                         gameWeaponPower = authoring.meleeWeaponPower,
                         WeaponPower = authoring.meleeWeaponPower,
                         hitPower = authoring.hitPower
