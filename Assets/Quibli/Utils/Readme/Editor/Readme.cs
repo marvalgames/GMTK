198bac82c3e1d3ab5e70df8cf79535d74f2c6165
Francisco Ramos <blbramos@gmail.com>
2024-07-22 20:26:55 -0400

    .

--- Assets/Quibli/Utils/Readme/Editor/Readme.cs
+++ Assets/Quibli/Utils/Readme/Editor/Readme.cs
@@ -16,7 +16,7 @@ namespace Quibli {
 [ExecuteAlways]
 public class Readme : ScriptableObject {
     [NonSerialized]
-    public readonly string AssetVersion = "2.5.0";
+    public readonly string AssetVersion = "2.5.5";
     [NonSerialized]
     public bool? UrpInstalled;
     [NonSerialized]
