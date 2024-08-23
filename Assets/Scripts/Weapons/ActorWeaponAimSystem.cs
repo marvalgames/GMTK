38c4d6138c6135e8bcce9a91b871522109a96a2f
Francisco Ramos <blbramos@gmail.com>
2024-08-23 16:54:49 -0400

    .

--- Assets/Scripts/Weapons/ActorWeaponAimSystem.cs
+++ Assets/Scripts/Weapons/ActorWeaponAimSystem.cs
@@ -16,7 +16,6 @@ public partial class EnemyWeaponAimSystemLateUpdate : SystemBase
             .ForEach((in EnemyWeaponAim mb, in ActorWeaponAimComponent actorWeaponAimComponent) =>
             {
                 mb.weaponRaised = actorWeaponAimComponent.weaponRaised == WeaponMotion.Started;
-                mb.weaponRaised = actorWeaponAimComponent.weaponRaised == WeaponMotion.Started;
                 mb.LateUpdateSystem();
             }).Run();
     }
@@ -37,34 +36,9 @@ public partial class PlayerWeaponAimSystemLateUpdate : SystemBase
             playerWeaponAimComponent.aimDirection = mb.aimDir;
             var direction = math.normalize(mb.aimDir);
             direction.y = 0;
-
-
-            var forwardVector = math.forward(localTransform.Rotation);
-            forwardVector.y = 0;
-            var degrees = Vector3.SignedAngle(forwardVector, direction, Vector3.up);
-            var turnSpeed = mb.turnSpeed;
-
-            if (math.abs(degrees - playerWeaponAimComponent.angleToTarget) < .03)
-            {
-                degrees = 0;
-            }
-
-            playerWeaponAimComponent.angleToTarget = degrees;
-            var turningValue = math.sign(degrees);
-            var slerpDampTime = mb.rotateSpeed;
-            turningValue = 1;
-
-            if (playerWeaponAimComponent.aimMode == false)
-            {
-                turningValue = 0;
-                slerpDampTime = 0;
-            }
-
+            
             var targetRotation = quaternion.LookRotationSafe(direction, math.up()); //always face xHair
-            //localTransform.Rotation = math.slerp(localTransform.Rotation, targetRotation.value,
-            //    slerpDampTime * SystemAPI.Time.DeltaTime);
             localTransform.Rotation = targetRotation;
-            mb.animator.SetFloat(Turning, turningValue, turnSpeed, SystemAPI.Time.DeltaTime);
         }).Run();
     }
 }
